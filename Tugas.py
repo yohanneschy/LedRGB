3ebbfe5b0695f80ad63cef1006db4ccce6a3f8dd
yohanneschy <32201713+yohanneschy@users.noreply.github.com>
2017-09-27 09:57:28 +0800

    Add files via upload

---
+++ Tugas.py
@@ -0,0 +1,73 @@
+import sys, os, glob
+from PyQt4 import QtCore, QtGui, uic
+from Tugaseee import Ui_MainWindow
+import serial, time
+import serial.tools.list_ports
+
+qtCreatorFile = "Tugas.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class Aplikasi(QtGui.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.pushButton_Openserial.clicked.connect(self.openserial)
+        self.pushButton_Hapus.clicked.connect(self.hapus)
+        self.pushButton_Keluar.clicked.connect(self.keluar)
+        self.textEdit_Hasil.append("Pengaturan LED RGB")
+        self.textEdit_Hasil.append(" ")
+
+        self.horizontalSlider_Merah.valueChanged.connect(self.Merah)   
+        self.horizontalSlider_Hijau.valueChanged.connect(self.Hijau)        
+        self.horizontalSlider_Biru.valueChanged.connect(self.Biru)
+        self.TXdata = bytearray(3)
+        
+    def Merah(self):
+        nilaiMerah  = self.horizontalSlider_Merah.value()   
+        self.textEdit_Hasil.append("merah = %d" %(nilaiMerah))
+        self.TXdata[0]= bytes(b'R')
+        self.TXdata[1]= nilaiMerah
+        self.ser.write(self.TXdata)
+
+    def Hijau(self):
+        nilaiHijau = self.horizontalSlider_Hijau.value()
+        self.textEdit_Hasil.append("hijau = %d" %(nilaiHijau))
+        self.TXdata[0]= bytes(b'G')
+        self.TXdata[1]=nilaiHijau
+        self.ser.write(self.TXdata)
+
+    def Biru(self):
+        nilaiBiru   = self.horizontalSlider_Biru.value()
+        self.textEdit_Hasil.append("biru = %d" %(nilaiBiru))
+        self.TXdata[0]= bytes(b'B')
+        self.TXdata[1]=nilaiBiru
+        self.ser.write(self.TXdata)
+        
+    def openserial(self):
+                try:
+                        arduino_ports = [
+                                a.device
+                                for a in serial.tools.list_ports.comports()
+                                if 'Arduino Uno' in a.description
+                                ]
+
+                        self.ser = serial.Serial(arduino_ports[0], 9600, timeout=0.1)
+                        time.sleep(1)
+                        self.textEdit_Hasil.append("Serial Port Terbuka")
+                            
+                except:
+                       if not arduino_ports:
+                        self.textEdit_Hasil.append('Arduino tidak terpasang')
+                        
+    def hapus(self):
+        self.textEdit_Hasil.clear()
+        
+    def keluar(self):
+        sys.exit()
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    window = Aplikasi()
+    window.show()
+    sys.exit(app.exec_())
---
+++ Tugas/Tugas.ino
@@ -0,0 +1,43 @@
+//yohannes CHy
+//11020
+
+int NilaiMerah;   
+int NilaiHijau;
+int NilaiBiru;
+const int led1 = 9;   //merah
+const int led2 = 10; //hijau
+const int led3 = 11; //biru
+byte RXBuf[2];
+
+
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);  
+  Serial.begin(9600);   //open serial port
+ }
+ 
+ void Nyala_led(int NilaiMerah, int NilaiHijau, int NilaiBiru){
+   analogWrite(led1, ~NilaiMerah); //  R
+   analogWrite(led2, ~NilaiHijau); //  B
+   analogWrite(led3, ~NilaiBiru); //  G
+    }
+    
+void loop(){
+  Nyala_led(NilaiMerah,NilaiHijau,NilaiBiru);
+  if (Serial.available()){
+   int nBytes = Serial.readBytes(RXBuf,sizeof(RXBuf));
+   if (nBytes>=2){
+//    NilaiMerah = int(RXBuf[0]);
+//    NilaiHijau = int(RXBuf[1]);
+//    NilaiBiru  = int(RXBuf[2]);
+    if (RXBuf[0]=='R'){NilaiMerah = int(RXBuf[1]);}
+    if (RXBuf[0]=='G'){NilaiHijau = int(RXBuf[1]);}
+    if (RXBuf[0]=='B'){NilaiBiru = int(RXBuf[1]);}
+    }
+ }
+}
+
+
+
